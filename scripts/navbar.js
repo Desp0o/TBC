9f5157ff54b64f9a6b9de12c95575b237bbd9225
Despo <tornike.despotashvili@gmail.com>
2024-01-18 23:08:20 +0400

    add animation for pannel and overlay bg

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -8,3 +8,13 @@ btn.addEventListener("click", () => {
     btn.classList.toggle("open");
   });
 });
+
+//animation for responsive pannel and overlay bg
+const burgerBtn = document.querySelector(".burger-menu")
+const resMenuPanel = document.querySelector(".res_menu_panel")
+const overlay = document.querySelector(".overlay")
+
+burgerBtn.addEventListener("click", ()=> {
+  resMenuPanel.classList.toggle("open")
+  overlay.classList.toggle("open")
+})
