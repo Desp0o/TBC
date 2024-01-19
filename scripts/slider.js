672144795f8d05f1175bf9c06058e03a0afa4fe4
Despo <tornike.despotashvili@gmail.com>
2024-01-19 20:46:09 +0400

    add dots and functionality

--- scripts/slider.js
+++ scripts/slider.js
@@ -4,6 +4,25 @@ let index = 0
 
 const leftBtn = document.getElementById('leftBtn')
 const rightBtn = document.getElementById('rightBtn')
+const dot1 = document.getElementById('dot1')
+const dot2 = document.getElementById('dot2')
+const dot3 = document.getElementById('dot3')
+
+const dotsArr = [dot1, dot2, dot3]
+
+dotsArr.forEach( (dot, dotIndex) => {
+    dot.addEventListener("click", () => {
+        updateSlider(dotIndex)
+    })
+})
+
+const updateSlider = (updatedIndex) => {
+    for (let i = 0; i < slider.length; i++) {
+        slider[i].classList.remove('active_slide');
+    }
+
+    slider[updatedIndex].classList.add('active_slide');
+};
 
 const nextSlide = () => {
     index++
