543bdb9318013fd17017597d79b2144980395751
Despo <tornike.despotashvili@gmail.com>
2024-01-19 20:27:56 +0400

    add prev/next btn for slide switch

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,6 +2,9 @@ const slider = document.querySelector('.slider').children
 const totalSlides = slider.length
 let index = 0
 
+const leftBtn = document.getElementById('leftBtn')
+const rightBtn = document.getElementById('rightBtn')
+
 const nextSlide = () => {
     index++
 
@@ -16,3 +19,22 @@ const nextSlide = () => {
     slider[index].classList.add('active_slide')
 }
 
+const prevSlide = () => {
+    
+    if(index === 0){
+        index = totalSlides - 1
+    }else{
+        index--
+    }
+
+    for (let i = 0; i < slider.length; i++) {
+        slider[i].classList.remove('active_slide')
+    }
+
+    slider[index].classList.add('active_slide')
+}
+
+leftBtn.addEventListener("click", prevSlide)
+rightBtn.addEventListener("click", nextSlide)
+
+setInterval(nextSlide, 3000);
