83f1f4b00f91463c9bd1712158ef54261e3f70a5
Despo <tornike.despotashvili@gmail.com>
2024-01-20 22:12:49 +0400

    body scroll on privacy click action / privacy width adjust

--- scripts/privacy.js
+++ scripts/privacy.js
@@ -1,3 +1,4 @@
+const docuBody = document.body
 const privacyPanel = document.querySelector('.privacy')
 const openPrivacy = document.querySelector('.footer_privacy_p')
 const closePrivacy = document.querySelector('.privacy_panel_close_icon')
@@ -7,19 +8,23 @@ const privacyOverlay = document.querySelector('.privacy_overlay')
 openPrivacy.addEventListener("click", () => {
     privacyPanel.classList.add('open')
     privacyOverlay.classList.add('open')
+    docuBody.classList.add('overflow_Hiddne')
 })
 
 closePrivacy.addEventListener("click", () => {
+    docuBody.classList.remove('overflow_Hiddne')
     privacyPanel.classList.remove('open')
     privacyOverlay.classList.remove('open')
 })
 
 closePrivacyBTn.addEventListener("click", () => {
+    docuBody.classList.remove('overflow_Hiddne')
     privacyPanel.classList.remove('open')
     privacyOverlay.classList.remove('open')
 })
 
 privacyOverlay.addEventListener("click", () => {
+    docuBody.classList.remove('overflow_Hiddne')
     privacyPanel.classList.remove('open')
     privacyOverlay.classList.remove('open')
 })
