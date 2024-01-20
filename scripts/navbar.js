0ad80205b9d273128936eec5a16d205f86a45b94
Despo <tornike.despotashvili@gmail.com>
2024-01-20 18:52:11 +0400

    scroll animations on responsive

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -19,12 +19,40 @@ burgerBtn.addEventListener("click", ()=> {
   overlay.classList.toggle("open")
 })
 
-// transparent
 
+///////// scrolling animations
+let lastScrollPosition = 0;
 window.addEventListener("scroll", () => {
-  if(window.scrollY > 0) {
-    console.log(0);
-  }
 
-  window.scrollY > 0 ? navbar.classList.add('transparent') : navbar.classList.remove('transparent')
+  window.scrollY > 0 ? navbar.classList.add('transparent') : navbar.classList.remove('transparent') //add transparent to nav on scroll
+
+  //check if window width is less then 700px 
+  if(window.innerWidth < 700){
+
+    //if above is everythin OK then hide nav when scrollY is more then 69
+    if(window.scrollY > 69){
+      navbar.classList.add('hide')
+    }else{
+      navbar.classList.remove('hide')
+    }
+
+    //check window scroll to show nav on scroll up
+    let currentScrollPos = document.documentElement.scrollTop;
+    if (currentScrollPos < lastScrollPosition) {
+      navbar.classList.remove('hide')
+    }
+
+    lastScrollPosition = currentScrollPos;
+
+    //if we want to appear nav when page refreshed
+    window.onload =()=>{
+      navbar.classList.remove('hide')
+    }
+
+    //when we reach MAX bottom side nav appears
+    const scrolledBottom = document.documentElement.scrollHeight - window.innerHeight;
+    if(window.scrollY === scrolledBottom){
+      navbar.classList.remove('hide')
+    }
+  }
 })
