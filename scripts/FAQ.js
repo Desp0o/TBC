21f0b7bf6a05281cf3bbb82828513b287ce443ea
Despo <tornike.despotashvili@gmail.com>
2024-01-20 17:15:27 +0400

    update FAQ with arrows / update footer

--- scripts/FAQ.js
+++ scripts/FAQ.js
@@ -1,24 +1,28 @@
-const faqItems = document.querySelectorAll(".faq_item");
+const faqItems = document.querySelectorAll(".faq_item")
+const faqArrows = document.querySelectorAll(".faq_arrow")
 
-faqItems.forEach((item) => {
+faqItems.forEach((item, index) => {
 
   item.addEventListener("click", () => {
 
-    faqItems.forEach((openedItem) => {
+    faqItems.forEach((openedItem, openedIndex) => {
 
       if (openedItem !== item && openedItem.classList.contains("open")) {
         openedItem.classList.remove("open");
         openedItem.removeAttribute("style");
+
+        faqArrows[openedIndex].classList.remove('expanded')
       }
       
     });
 
-    item.classList.toggle("open");
+    item.classList.toggle("open")
+    faqArrows[index].classList.toggle('expanded')
 
     if (item.classList.contains("open")) {
-      item.style.height = `${item.scrollHeight}px`;
+      item.style.height = `${item.scrollHeight}px`
     } else {
-      item.removeAttribute("style");
+      item.removeAttribute("style")
     }
   });
 });
