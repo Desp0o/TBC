d731ee7c7e08fe87f7e9ebf4c4a2278d23e48e8f
Despo <tornike.despotashvili@gmail.com>
2024-01-20 21:18:05 +0400

    add overlay to privacy panel and removed hover effect on button in pruvacy panel

--- scripts/privacy.js
+++ scripts/privacy.js
@@ -2,15 +2,24 @@ const privacyPanel = document.querySelector('.privacy')
 const openPrivacy = document.querySelector('.footer_privacy_p')
 const closePrivacy = document.querySelector('.privacy_panel_close_icon')
 const closePrivacyBTn = document.querySelector('.privacy_close_btn')
+const privacyOverlay = document.querySelector('.privacy_overlay')
 
 openPrivacy.addEventListener("click", () => {
     privacyPanel.classList.add('open')
+    privacyOverlay.classList.add('open')
 })
 
 closePrivacy.addEventListener("click", () => {
     privacyPanel.classList.remove('open')
+    privacyOverlay.classList.remove('open')
 })
 
 closePrivacyBTn.addEventListener("click", () => {
     privacyPanel.classList.remove('open')
+    privacyOverlay.classList.remove('open')
+})
+
+privacyOverlay.addEventListener("click", () => {
+    privacyPanel.classList.remove('open')
+    privacyOverlay.classList.remove('open')
 })
