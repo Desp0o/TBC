a12a36d6cec86798b7e502e80f4d77d74aa00449
Despo <tornike.despotashvili@gmail.com>
2024-01-23 00:30:31 +0400

    UPDATE... add touch event on slider to change slides

--- scripts/slider.js
+++ scripts/slider.js
@@ -5,6 +5,9 @@ let currentIndex = 0;
 let interval; // We need this for interval, to start and clear it,otherwise we may miss any slide on click events
 let isPaused = false 
 let pauseHelper = true
+let startPosX = 0  //touch start position 
+let endPosX = 0 //touch end position
+const threshold = 50 //treshold for compare calculated distance
 
 const leftBtn = document.getElementById('leftBtn');
 const rightBtn = document.getElementById('rightBtn');
@@ -83,11 +86,30 @@ rightBtn.addEventListener("click", () => {
 
 
 // to stop slider on touch slider main container like in TBC ACADEMY web page
-sliderContainer.addEventListener('touchstart', () => {
+sliderContainer.addEventListener('touchstart', (event) => {
     isPaused = true
     pauseHelper = isPaused
     clearInterval(interval)
+
+    startPosX = event.touches[0].clientX;
 })
+
+sliderContainer.addEventListener('touchmove', (event) => {
+    event.preventDefault()
+})
+
+sliderContainer.addEventListener('touchend', (event) => {
+    const endPosX = event.changedTouches[0].clientX;
+    const distance = startPosX - endPosX
+
+    if(distance > threshold){
+        nextSlide()
+    }else if (distance < -threshold){
+        prevSlide()
+    }
+})
+
+
 
 
 // resume slider when touched outside of slider main container
